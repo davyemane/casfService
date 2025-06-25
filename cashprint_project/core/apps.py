202275c2adcf542089735a9b396c7768f4da895a
Emane Bile Felicien Davy <82854992+davyemane@users.noreply.github.com>
2025-06-25 09:44:06 +0100

    Delete cashprint_project/core directory

--- cashprint_project/core/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-    verbose_name = 'Cash Print - Core'
-    
-    def ready(self):
-        import core.signals  # Importer les signaux
